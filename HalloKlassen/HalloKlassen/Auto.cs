77cc40ce608a36ec7cde12e82af8d1ff86cdbdaa
André <AndreR@ppedv.de>
2021-04-07 10:48:36 +0200

    Methoden und Funktionen

---
+++ HalloKlassen/HalloKlassen/Auto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HalloKlassen
+{
+    class Auto
+    {
+        public string Hersteller;
+        public string Modell;
+        public int PS;
+        public string Farbe;
+    }
+}
---
+++ HalloKlassen/HalloKlassen/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace HalloKlassen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            SagHallo();
+            string derName = "Wilma";
+            SagHallo(derName, 76);
+
+            int zahl = GibMirFünf();
+            Console.WriteLine(zahl);
+            int dasDoppelte = Verdoppeln(Verdoppeln(Verdoppeln(Verdoppeln(Verdoppeln(zahl)))));
+            Console.WriteLine(dasDoppelte);
+
+        }
+
+        static void SagHallo() //einfache Methode (überladen)
+        {
+            Console.WriteLine("Hallo");
+        }
+
+        static void SagHallo(string name, int alter) //methode mit Parameter (überladen)
+        {
+            Console.WriteLine($"Hallo {name} {alter}");
+        }
+
+        static int GibMirFünf()
+        {
+            return 5;
+        }
+        static int Verdoppeln(int zahl)
+        {
+            return zahl * 2;
+        }
+
+
+    }
+}
